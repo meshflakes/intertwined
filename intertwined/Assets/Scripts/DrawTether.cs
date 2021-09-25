34da8d923f12e12ed275dea99ba52ec103131019
Michelle <michelle.kee@mail.utoronto.ca>
2021-09-25 12:02:52 -0400

    Tether script

---
+++ intertwined/Assets/Scripts/DrawTether.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawTether : MonoBehaviour
+{
+    private LineRenderer lr;
+    private float counter;
+    private float dist;
+    
+    public Transform p1;
+    public Transform p2;
+
+    public float drawSpeed = 6f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lr = GetComponent<LineRenderer>();
+        lr.SetPosition(0, p1.position);
+        lr.SetWidth(.15f, .15f);
+        
+        
+        dist = Vector3.Distance(p1.position, p2.position);
+        //dist = Vector3.Distance(p1.position, p2.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (counter <dist){
+            counter += .1f/drawSpeed;
+            //float x = Mathf.Lerp(0, dist, counter);
+            Vector3 pointA = p1.position;
+            Vector3 pointB = p2.position;
+            float x = Mathf.Lerp(0, Vector3.Distance(pointA, pointB), counter);
+            Vector3 pointAlongLine = x * Vector3.Normalize(pointB-pointA) + pointA;
+            lr.SetPosition(0, pointA);
+            lr.SetPosition(1, pointAlongLine);
+            
+            //}
+    }
+}
