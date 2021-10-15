3607bfa4d45291bd19e2dc236babd0a2d48e8f5d
meshflakes <animesh1997@live.com>
2021-10-15 13:35:09 -0400

    Cleaned up key & door interaction. Object is verified to as a key before unlocking, and key can define the lockIds it can unlock.

--- intertwined/Assets/Scripts/Interactable/GrabbableInteractable.cs
+++ intertwined/Assets/Scripts/Interactable/GrabbableInteractable.cs
@@ -5,43 +5,69 @@ namespace Interactable
 {
     public abstract class GrabbableInteractable : Interactable
     {
-        private GameObject _heldBy;
+        private Character.Character _heldByCharacter;
+        private Transform _heldByTransform;
+
+        private bool _hasRigidBody;
         private Rigidbody _rigidbody;
+        private Transform _transform;
 
         private Vector3 _holdingOffset = new Vector3(1, 1, 0);
         
         // TODO: may need to add holding positions / animations
         
-        private void Start()
+        protected void Start()
         {
+            _transform = gameObject.GetComponent<Transform>();
             _rigidbody = gameObject.GetComponent<Rigidbody>();
+            _hasRigidBody = _rigidbody != null;
         }
 
         protected bool Held()
         {
-            return _heldBy != null;
+            return _heldByCharacter != null;
         }
 
         public void Grab(GameObject obj)
         {
-            _heldBy = obj;
-            obj.GetComponent<Character.Character>().CharInteractor.HeldInteractable = this;
-            _rigidbody.freezeRotation = true;
-            _rigidbody.position = _heldBy.GetComponent<Transform>().position + _holdingOffset;
-            _rigidbody.useGravity = false;
+            Debug.Log("grabbing object");
+            
+            _heldByCharacter = obj.GetComponent<Character.Character>();
+            _heldByTransform = obj.GetComponent<Transform>();
+            
+            _heldByCharacter.CharInteractor.HeldInteractable = this;
+            
+            if (_hasRigidBody)
+            {
+                _rigidbody.freezeRotation = true;
+                _rigidbody.position = _heldByTransform.position + _holdingOffset;
+                _rigidbody.useGravity = false;
+            }
         }
 
         public void Release()
         {
-            _heldBy.GetComponent<Character.Character>().CharInteractor.HeldInteractable = null;
-            _heldBy = null;
-            _rigidbody.freezeRotation = false;
-            _rigidbody.useGravity = true;
+            _heldByCharacter.CharInteractor.HeldInteractable = null;
+            _heldByCharacter = null;
+            _heldByTransform = null;
+            
+            if (_hasRigidBody)
+            {
+                _rigidbody.freezeRotation = false;
+                _rigidbody.useGravity = true;
+            }
         }
 
         public void UpdatePosition()
         {
-            _rigidbody.MovePosition(_heldBy.GetComponent<Transform>().position + _holdingOffset);
+            if (_hasRigidBody)
+            {
+                _rigidbody.MovePosition(_heldByTransform.position + _holdingOffset);
+            }
+            else
+            {
+                _transform.position = _heldByTransform.position + _holdingOffset;
+            }
         }
     }
 }
--- intertwined/Assets/Scripts/Interactable/KeyInteractable.cs
+++ intertwined/Assets/Scripts/Interactable/KeyInteractable.cs
@@ -1,13 +1,31 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace Interactable
 {
     public class KeyInteractable : GrabbableInteractable, KeyType
     {
-        public bool CanUnlock(int keyId)
+        [Tooltip("List of lock ids that this key can unlock")]
+        public List<int> unlockableLockIds;
+
+        private HashSet<int> _unlockableLockIdsSet;
+
+        public new void Start()
         {
-            // TODO: add proper logic for unlock
-            return keyId != 0;
+            base.Start();
+            if (unlockableLockIds != null)
+            {
+                _unlockableLockIdsSet = new HashSet<int>(unlockableLockIds);
+            }
+            else
+            {
+                _unlockableLockIdsSet = new HashSet<int>();
+            }
+        }
+
+        public bool CanUnlock(int lockId)
+        {
+            return _unlockableLockIdsSet.Contains(lockId);
         }
 
         public override bool UsedWith(Interactable other)
