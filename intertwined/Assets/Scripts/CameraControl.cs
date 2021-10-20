f9d8baf8fd2753d717f49773c257ef899e968ace
MatEdwards <matt.ellisedwards@gmail.com>
2021-10-20 16:42:26 -0400

    Added basic camera implementation
    uses only Mouse 1 to rotate
    TODO: switch to trigger based rotation or add gamepad rotation

--- intertwined/Assets/Scripts/CameraControl.cs
+++ intertwined/Assets/Scripts/CameraControl.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,9 +7,11 @@ public class CameraControl : MonoBehaviour
 {
     public Transform p1;
     public Transform p2;
+    public Collider camTrigger;
 
     public float zoom = 0.165f;
     public float zoomStartDist = 6;
+    public float maxDist = 12;
     public float _followTimeDelta = 0.8f;
     public float smoothness = 0.5f;
     
@@ -19,13 +22,26 @@ public class CameraControl : MonoBehaviour
     void Start()
     {
         _cameraOffset = transform.position - ((p1.position + p2.position) / 2f);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButton(0))
+        {
+            rotateCam();
+        }
+        transform.LookAt((p1.position + p2.position) / 2f);
+
         Vector3 midpoint = (p1.position + p2.position) / 2f;
         float distance = (p1.position - p2.position).magnitude;
+        if (distance > maxDist)
+        {
+            distance = maxDist;
+        }
+        
+        
         if (distance > zoomStartDist)
         {
             newPos = midpoint + _cameraOffset * distance * zoom;
@@ -34,9 +50,14 @@ public class CameraControl : MonoBehaviour
         {
             newPos = midpoint + _cameraOffset;
         }
-        
 
         transform.position = Vector3.Slerp(transform.position, newPos, smoothness);
+        
+        
+    }
 
+    public void rotateCam()
+    {
+        _cameraOffset = Quaternion.AngleAxis(0.2f, Vector3.up) * _cameraOffset;
     }
 }
