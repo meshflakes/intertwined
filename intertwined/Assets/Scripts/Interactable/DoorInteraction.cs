03d8868d0199e38efb9d255f47352751dc8d7c39
meshflakes <animesh1997@live.com>
2021-11-09 19:47:07 -0500

    Added door rotation animation

--- intertwined/Assets/Scripts/Interactable/DoorInteraction.cs
+++ intertwined/Assets/Scripts/Interactable/DoorInteraction.cs
@@ -1,39 +1,46 @@
+using System;
 using Interactable;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class DoorInteraction : Interactable.Interactable
 {
-    private bool _opened = false;
     private Transform _transform;
     private Quaternion _closedRotation;
     private Quaternion _openRotation;
     private Quaternion _transitionRotationPerUpdate;
     private Vector3 _pivotPosition;
-    
+    private bool Swinging => _remainingRotation != 0;
+    private float _remainingRotation = 0f;
+    private Vector3 _rotationAxis = Vector3.zero;
+    private int _rotationAxisModifier;        
+        
     [Header("Door")]
     [Tooltip("How many degrees the door opens on rotation")]
     [Range(0, 359)]
-    public int Rotation = 90;
+    public int rotation = 90;
     [Tooltip("How fast the door rotates")] 
-    public int DegreesRotatedPerSecond = 20;
+    public int degreesRotatedPerSecond = 20;
 
     [Space(10)]
     [Tooltip("Starting 'unlocked' status of the door")]
-    public bool Unlocked = true;
+    public bool unlocked = true;
     [Tooltip("Id of key required to open the door, 0 if no key is required")]
-    public int LockId;
+    public int lockId;
     
     [Space(10)]
     [Tooltip("Starting state of the door")]
-    public bool Open = false;
+    public bool open = false;
     [Tooltip("Toggle direction of opening the door")]
-    public bool OpeningTowardsNorth = true;
+    public bool openingTowardsNorth = true;
+
+    
     
     void Start()
     {
         _transform = gameObject.GetComponent<Transform>();
         _pivotPosition = _transform.Find("DoorPivot").position;
+        _rotationAxisModifier = openingTowardsNorth ? 1 : -1;
         // var rotation = _transform.rotation;
         // _transform.po
         //
@@ -44,21 +51,32 @@ public class DoorInteraction : Interactable.Interactable
         //     rotation.w);
     }
 
+    private void Update()
+    {
+        if (!Swinging) return;
+        
+        var degreesToRotate = Math.Min(Time.deltaTime * degreesRotatedPerSecond, _remainingRotation);
+        _transform.RotateAround(_pivotPosition, _rotationAxis, degreesToRotate);
+        _remainingRotation -= degreesToRotate;
+    }
+
     public override bool Interact(Character.Character interacter)
     {
-        if (!Unlocked) return false;
+        if (!unlocked || Swinging) return false;
         
-        if (!_opened)
+        if (!open)
         {
             Debug.Log("opening");
-            _opened = true;
-            _transform.RotateAround(_pivotPosition, Vector3.up, Rotation);
+            open = true;
+            _remainingRotation = rotation;
+            _rotationAxis = Vector3.up * _rotationAxisModifier;
         }
         else
         {
             Debug.Log("closing");
-            _opened = false;
-            _transform.RotateAround(_pivotPosition, Vector3.down, Rotation);
+            open = false;
+            _remainingRotation = rotation;
+            _rotationAxis = Vector3.down * _rotationAxisModifier;
         }
 
         return true;
@@ -66,12 +84,12 @@ public class DoorInteraction : Interactable.Interactable
     
     public override bool Interact(Character.Character interacter, Interactable.Interactable interactable)
     {
-        if (LockId == 0 || Unlocked) return Interact(interacter);
+        if (lockId == 0 || unlocked) return Interact(interacter);
 
         // check if Interactable is a key AND the key can unlock this door
-        if (interactable is KeyType key && key.CanUnlock(LockId))
+        if (interactable is KeyType key && key.CanUnlock(lockId))
         {
-            Unlocked = true;
+            unlocked = true;
             return Interact(interacter);
         }
         else return false;
