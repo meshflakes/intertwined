f5dce4c520afba40400c66858c98453ae8103897
meshflakes <animesh1997@live.com>
2021-11-19 23:42:59 -0500

    Merged commit: Removed collisions between boy & dog

---
+++ intertwined/Assets/Scripts/Character/BlockCharacterCollision.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Character
+{
+    /**
+     * Disable collisions between the character's collider & its character collision blocker collider
+     *
+     * reference: https://www.youtube.com/watch?v=-yjKyI8NfKA
+     */
+    public class BlockCharacterCollision : MonoBehaviour
+    {
+        public Collider characterCollider;
+        public Collider characterCollisionBlocker;
+        
+        protected void Start()
+        {
+            Physics.IgnoreCollision(characterCollider, characterCollisionBlocker, true);
+        }
+    }
+}
