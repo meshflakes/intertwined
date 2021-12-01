78e10cee2adad14f5cdcb8405b77ae3935bd05bb
Michelle Kee <michelle.hc.kee@gmail.com>
2021-12-01 15:56:54 -0500

    Smooth lighting transitions

--- intertwined/Assets/Scripts/Anxiety/AnxietyLighting.cs
+++ intertwined/Assets/Scripts/Anxiety/AnxietyLighting.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
 
 public class AnxietyLighting : MonoBehaviour
 {
@@ -9,6 +10,8 @@ public class AnxietyLighting : MonoBehaviour
     public Light boyLight;
 
     public Light dogLight;
+
+    private bool crRunning = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,26 +19,35 @@ public class AnxietyLighting : MonoBehaviour
         boyLight.intensity = 0f;
         dogLight.intensity = 0f;
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    
     //This function is for lighting effects when the characters are too far apart
     //Level can be 1-7, determining how far
     public void farLighting(float level)
     {
-        directionalLight.intensity = 0f;
+        if(!crRunning) StartCoroutine(DirectionalLightTransition(directionalLight, 1f, 0f));
         boyLight.intensity = 8f-level;
         dogLight.intensity = 8f-level;
     }
 
     public void normalLighting()
     {
-        directionalLight.intensity = 1f;
+        if(!crRunning) StartCoroutine(DirectionalLightTransition(directionalLight, 1f, 1f));
         boyLight.intensity = 0f;
         dogLight.intensity = 0f;
+    }
+    
+    IEnumerator DirectionalLightTransition(Light l, float duration, float targetIntensity)
+    {
+        crRunning = true;
+        float startIntensity = l.intensity ;
+        float currentTime = 0;
+        while (currentTime < duration)
+        {
+            currentTime += Time.deltaTime;
+            l.intensity = Mathf.Lerp(startIntensity, targetIntensity, currentTime / duration);
+            yield return null;
+        }
+        crRunning = false;
+        yield break;
     }
 }
