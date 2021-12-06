2d61f5fa9a4cd202ce80353840968ee21f2e5087
meshflakes <animesh1997@live.com>
2021-12-05 23:35:23 -0500

    Elevator, Crane, Generator completed

--- intertwined/Assets/Scripts/Interactable/ConstructionSite/Crane.cs
+++ intertwined/Assets/Scripts/Interactable/ConstructionSite/Crane.cs
@@ -1,7 +1,59 @@
-﻿namespace Interactable.ConstructionSite
+﻿using System;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Interactable.ConstructionSite
 {
-    public class Crane
+    public class Crane : MonoBehaviour
     {
-        
+        [NonSerialized] public bool Powered = false;
+        [NonSerialized] public int NumInTrigger = 0;
+        public int degreesRotatedPerSecond;
+        public float maxRotation;
+        private float _currentRotation = 27f;
+        [FormerlySerializedAs("barBlockingColliders")] public GameObject frontBlockingColliders;
+        public GameObject backBlockingColliders;
+        public Transform craneTransform;
+
+        protected void Update()
+        {
+            UpdateRotation();
+            UpdateSteelBeamColliders();
+        }
+
+        private void UpdateRotation()
+        {
+            if (!Powered) return;
+                        
+            float degreesToRotate;
+            
+            if (NumInTrigger > 0)
+            {
+                if (IsEqual(_currentRotation, maxRotation)) return;
+                
+                degreesToRotate = Math.Min(Time.deltaTime * degreesRotatedPerSecond, maxRotation-_currentRotation);
+                craneTransform.Rotate(0, degreesToRotate, 0);
+                _currentRotation += degreesToRotate;
+                return;
+            }
+            else if (IsEqual(_currentRotation, 0f)) return;
+            
+            degreesToRotate = Math.Min(Time.deltaTime * degreesRotatedPerSecond, _currentRotation);
+            craneTransform.Rotate(0, -degreesToRotate, 0);
+            _currentRotation -= degreesToRotate;
+        }
+
+        private void UpdateSteelBeamColliders()
+        {
+            frontBlockingColliders.SetActive(!IsEqual(_currentRotation, 0));
+            backBlockingColliders.SetActive(!IsEqual(_currentRotation, maxRotation));
+        }
+
+        private static bool IsEqual(float x, float y)
+        {
+            const double epsilon = 1e-5;
+
+            return Math.Abs(x - y) < epsilon;
+        }
     }
 }
--- intertwined/Assets/Scripts/Interactable/ConstructionSite/Generator.cs
+++ intertwined/Assets/Scripts/Interactable/ConstructionSite/Generator.cs
@@ -1,7 +1,36 @@
 ﻿namespace Interactable.ConstructionSite
 {
-    public class Generator
+    public class Generator : Interactable
     {
+        public int lockId;
+        public Elevator elevator;
+        public Crane crane;
+
+        private bool _powered = false;
         
+        public override bool Interact(Character.Character interacter)
+        {
+            return false;
+        }
+
+        public override bool Interact(Character.Character interacter, Interactable interactable)
+        {
+            if (_powered) return false;
+            
+            if (interactable is KeyType key && key.CanUnlock(lockId))
+            {
+                _powered = true;
+                elevator.Powered = true;
+                crane.Powered = true;
+                return Interact(interacter);
+            }
+
+            return false;
+        }
+
+        public override bool UsedWith(Interactable other)
+        {
+            return true;
+        }
     }
 }
