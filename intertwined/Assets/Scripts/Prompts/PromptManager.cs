4ec568358bdc68416f1ce39760a159af9b31ff8d
meshflakes <animesh1997@live.com>
2021-12-06 20:03:22 -0500

    Reformatting

--- intertwined/Assets/Scripts/Prompts/PromptManager.cs
+++ intertwined/Assets/Scripts/Prompts/PromptManager.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using Character;
 using UnityEngine;
@@ -11,7 +10,6 @@ namespace Prompts
         private Transform _dogTransform;
         
         private Transform _canvasTransform;
-        private List<GameObject> _imagesList = new List<GameObject>();
 
         public List<Sprite> sprites;
 
@@ -19,8 +17,6 @@ namespace Prompts
         private float _boyPromptTimeout;
         private Prompt _dogCurrentPrompt;
         private float _dogPromptTimeout;
-
-        
         
         private void Start()
         {
@@ -31,12 +27,8 @@ namespace Prompts
         
         private void Update()
         {
-            
-            // TODO: if paused disable all images
-            
             DeleteExpiredPrompts();
             RepositionActivePrompts();
-
         }
 
         private void DeleteExpiredPrompts()
@@ -107,8 +99,8 @@ namespace Prompts
             } 
         }
 
-        //Destroy promtps that are related to anxiety if exists
-        public void DestoryAnxietyPrompts()
+        // Destroy prompts that are related to anxiety if they exist
+        public void DestroyAnxietyPrompts()
         {
             if (_boyCurrentPrompt != null && _boyCurrentPrompt.GetSprite() == sprites[(int) PromptType.Dog])
             {
