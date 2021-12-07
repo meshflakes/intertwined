49b0aba5a86f76eaa374737a62cfb035c97f216f
meshflakes <animesh1997@live.com>
2021-12-06 22:48:24 -0500

    Added new camera position for crane puzzle

---
+++ intertwined/Assets/Scripts/Camera/AreaCameraController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Controls;
+using UnityEngine;
+
+namespace Camera
+{
+    /**
+    * Slightly scuffed implementation, should extract common stuff from CameraSequenceController
+     * to another class and have both of these inherit from it instead
+    */
+    public class AreaCameraController : CameraSequenceController
+    {
+
+        [NonSerialized] public bool AreaCamActive = false;
+        
+        public AreaCameraController(Vector3 endingPosition, Quaternion endingRotation, Transform cameraTransform,
+            float animationDuration)
+            : base(endingPosition, endingRotation, cameraTransform, animationDuration, float.NaN)
+        {
+            // minimum time before return doesn't apply to this camera controller
+        }
+        
+        protected override bool CheckIfMoveBackToStart()
+        {
+            return !AreaCamActive;
+        }
+
+    }
+}
---
+++ intertwined/Assets/Scripts/Interactable/ConstructionSite/CraneCameraManager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Camera;
+using Interactable.Util;
+using UnityEngine;
+
+namespace Interactable.ConstructionSite
+{
+    
+    public class CraneCameraManager : MonoBehaviour
+    {
+        [Header("Camera Sequence")]
+        [Tooltip("Time taken for camera to move from original position to end position and vice versa")]
+        public float animationDuration = 2;
+        
+        private AreaCameraManager _cameraSequence;
+        [NonSerialized] public readonly List<int> NumInLocations = new List<int> {0, 0, 0};
+
+        protected void Start()
+        {
+            var targetTransform = transform.Find("Camera");
+            _cameraSequence = new AreaCameraManager(targetTransform, animationDuration);
+        }
+
+        protected void Update()
+        {
+            Debug.Log($"{NumInLocations[0]}, {NumInLocations[1]}, {NumInLocations[2]}");
+            if (NumInLocations[(int) Location.FullCraneArea] != 2)
+            {
+                // nobody in rn, make sure camera is not being modified
+                if (_cameraSequence.GetCameraActive())
+                {
+                    _cameraSequence.EndCameraSequence();
+                }
+                
+            } else if (NumInLocations[(int) Location.FinalBuilding] == 2)
+            {
+                // reached end, revert to regular camera
+
+                if (_cameraSequence.GetCameraActive()) _cameraSequence.EndCameraSequence();
+                else throw new Exception("Something went wrong here");
+            }
+            else
+            {
+                // set to special camera
+                Debug.Log("Trying to start sequence");
+                if (!_cameraSequence.GetCameraActive()) _cameraSequence.StartNewCameraSequence();
+                
+            }
+        }
+    }
+}
