6509580073242a24c6b1589926a714e88c2a8f3f
meshflakes <animesh1997@live.com>
2021-12-28 17:09:30 -0500

    Added GetInterface____<> type functions to Component via extension methods

---
+++ intertwined/Assets/Scripts/Extensions/ComponentExtension.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Extensions
+{
+    // Extension methods for the component class
+    // https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/extension-methods#example
+    
+    public static class ComponentExtension
+    {
+        public static T GetInterface<T>(this GameObject inObj) where T : class
+        {
+            return inObj.GetInterfaces<T>().FirstOrDefault();
+        }
+ 
+        public static IEnumerable<T> GetInterfaces<T>(this GameObject inObj) where T : class
+        {
+            if (!typeof(T).IsInterface) {
+                Debug.LogError(typeof(T).ToString() + ": is not an actual interface!");
+                return Enumerable.Empty<T>();
+            }
+ 
+            return inObj.GetComponents<Component>().OfType<T>();
+        }
+        
+        public static T GetInterfaceInChildren<T>(this GameObject inObj) where T : class
+        {
+            return inObj.GetInterfacesInChildren<T>().FirstOrDefault();
+        }
+        
+        public static IEnumerable<T> GetInterfacesInChildren<T>(this GameObject inObj) where T : class
+        {
+            if (!typeof(T).IsInterface) {
+                Debug.LogError(typeof(T).ToString() + ": is not an actual interface!");
+                return Enumerable.Empty<T>();
+            }
+ 
+            return inObj.GetComponentsInChildren<Component>().OfType<T>();
+        }
+    }
+}
